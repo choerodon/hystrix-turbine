0642149b20ba6ea3a01c43b37dc8002392b8d694
flyleft <jcalaz@163.com>
2018-11-22 12:01:55 +0800

    [IMP] 去除kafka依赖；升级starter版本至0.8.0；移除kafka相关的配置

--- src/main/java/io/choerodon/hystrix/HystrixTurbineApplication.java
+++ src/main/java/io/choerodon/hystrix/HystrixTurbineApplication.java
@@ -3,14 +3,14 @@ package io.choerodon.hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * @author dongfan117@gmail.com
  */
 @SpringBootApplication
 @EnableEurekaClient
-@EnableTurbineStream
+@EnableTurbine
 public class HystrixTurbineApplication {
     public static void main(String[] args) {
         SpringApplication.run(HystrixTurbineApplication.class, args);
